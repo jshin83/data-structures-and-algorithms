d3ffa784629cc2f157a1fbaea1543c3ec479976c
jshin83 <jen.kyongah.lee@gmail.com>
2019-05-07 15:32:50 -0700

    completed challenge 8

--- 2Darrays/challenges-12.test.js
+++ 2Darrays/challenges-12.test.js
@@ -192,7 +192,7 @@ CHALLENGE 8
 
 Write a function called excel that accepts a string representing rows and columns in a table.
 
-Rows are seperated by newline "\n" characters. Columns are seperated by commas. For example, '1,1,1\n4,4,4\n9,9,9' represents a 3x3 table.
+Rows are seperated by newline "\n" characters. Columns are separated by commas. For example, '1,1,1\n4,4,4\n9,9,9' represents a 3x3 table.
 
 The function should parse the string as rows and columns and compute the sum of the values for each row. Return an array with the sum of the values in each row.
 
@@ -201,6 +201,16 @@ For example, excel('1,1,1\n4,4,4\n9,9,9') returns [3, 12, 27].
 
 const excel = (str) => {
   // Solution code here...
+  let rows = str.split('\n');
+  rows.forEach( element => element = element.split(','));
+  for (let i = 0; i < rows.length; i++) {
+    let sum = 0;
+    for (let j = 0; j < rows[i].length; j++) {
+      if(rows[i][j] !== ',') sum += Number(rows[i][j]);
+    }
+    rows[i] = sum;
+  }
+  return rows;
 };
 
 /* ------------------------------------------------------------------------------------------------
